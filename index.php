870e7faea007199b567e56801a5face14483520b
Jean Admin <jeanadmin@Mac-mini-de-Seb.local>
2020-02-18 15:27:25 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no">
+    <link rel="stylesheet" href="css/styles.min.css">
+    
+<link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css" integrity="sha256-mmgLkCYLUQbXn0B1SRqzHar6dCnv9oZFPEC1g1cwlkk=" crossorigin="anonymous" />
+    <title>Code Names</title>
+
+</head>
+
+<body>
+<div class="wrapper">
+    JEU FINI ! 
+</div>
+<div class="view"><i class="fas fa-eye"></i></div>
+<script src="js/custom.min.js"></script>
+</body>
+
+</html>
+ 
